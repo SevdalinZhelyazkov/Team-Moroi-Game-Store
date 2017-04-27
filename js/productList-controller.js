ac35845edfef6482588a57375089483d75cdeb95
Sevdalin Zhelyazkov <irrealizable@abv.bg>
2017-04-27 17:10:23 +0300

    Code refactoring, updated gitignore

--- js/productList-controller.js
+++ js/productList-controller.js
@@ -3,17 +3,19 @@ import { templateLoader } from 'template-loader';
 import { productsData } from 'products-data';
 
 const productListController = {
-    show(context) return Promise
+    show(context) {
+        return Promise
             .all([
                 productsData.getProducts(),
                 templateLoader.load('product-list')
-                ])
+            ])
             .then((data) => {
                 const productsData = data[0];
                 const template = data[1];
 
                 $('.game-box-container').html(template(productsData));
-            }
+            });
+    }
 };
 
 export { playersController };
--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -9,22 +9,22 @@ class ShoppingCart {
 
     addGame(game) {
         if (this._games.indexOf(game) >= 0) {
-            //TO DO SET POPUP GAME ALREADY ON LIST
+            //TODO: SET POPUP GAME ALREADY ON LIST
         }
         this._games.push(game);
     }
     removeGame(game) {
         let index = this._games.findIndex(game);
-        if (index >= = 0) {
+        if (index >= 0) {
             this._games.splice(index, 1);
         }
     }
 
-    totalSum(){
-      let sum = 0;
-      for (product of this.games) {
-        sum = sum + product.price;
-      }
-      return sum;
+    totalSum() {
+        let sum = 0;
+        for (let product of this.games) {
+            sum = sum + product.price;
+        }
+        return sum;
     }
 }
