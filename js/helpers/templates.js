695273605b692618fb4e6eb15420879f0b7ef801
Sevdalin Zhelyazkov <irrealizable@abv.bg>
2017-05-08 21:28:19 +0300

    Added handlebars template loader

---
+++ js/helpers/templates.js
@@ -0,0 +1,30 @@
+import handlebars  from 'handlebars';
+import $ from 'jquery';
+
+class HandlebarsTemplates {
+    constructor(){
+        // this.cache = {};
+    }
+
+    getTemplate(name){
+        // var cache = this.cache;
+        // var _this = this;
+        return new Promise(function(resolve, reject){
+            // if(cache[name]){
+            //     resolve(cache[name]);
+            // } else {
+                $.get(`../../templates/${name}.handlebars`, function(templateHtml){
+                    var template = handlebars.compile(templateHtml);
+                    // cache[name] = template;
+                    resolve(template);
+                });
+                // _this.cache = cache;
+            // }
+        });
+        
+    }
+}
+
+let templates = new HandlebarsTemplates();
+
+export { templates as templates };
