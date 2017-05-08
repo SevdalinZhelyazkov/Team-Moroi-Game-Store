611248546c46dd4caf45f486947046cd285cc93d
Sevdalin Zhelyazkov <irrealizable@abv.bg>
2017-05-08 23:08:42 +0300

    Hotfix: project settings

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 SystemJS.config({
-    transpiler: 'plugin-babel',
-    map: {
+    'transpiler': 'plugin-babel',
+    'map': {
         'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
-        'jquery': './bower_components/jquery/dist/jquery.min.js',
+        'jquery': './bower_components/jquery/dist/jquery.js',
         'sammy': './bower_components/sammy/lib/sammy.js',
-        'handlebars': './bower_components/handlebars/handlebars.min.js',
+        'handlebars': './bower_components/handlebars/handlebars.js',
         'toastr':'./bower_components/toastr/toastr.js',
         'cryptojs': './bower_components/crypto-js/crypto-js.js',
-        
+
         'templates': './js/helpers/templates.js',
         'constants': './js//helpers/constants.js',
         'utils': './js/helpers/utils.js',
@@ -23,6 +23,7 @@ SystemJS.config({
         'requester': './js/requests/requester.js',
 
         'sammyApp': './js/sammyApp.js',
+
     }
 });
 
